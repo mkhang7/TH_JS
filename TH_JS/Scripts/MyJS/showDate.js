5bbce6c8d65651533462e05acb2f099845698ecd
admin <admin@DESKTOP-GRCDA3Q>
2023-10-05 23:58:35 +0700

    Add project files.

---
+++ TH_JS/Scripts/MyJS/showDate.js
@@ -0,0 +1,7 @@
+﻿function HienThiNgay() {
+    var _date = new Date;
+    var ngay = _date.getDay()+1;
+    var thang = _date.getMonth()+1;
+    var nam = _date.getFullYear();
+    alert("Hôm nay là ngày " + ngay + " tháng " + thang + " năm " + nam);
+}
